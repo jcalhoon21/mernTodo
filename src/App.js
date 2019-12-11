2a3f74724de8dba91496600306e38a785e23e28f
jcalhoon21 <jcalhoon21@gmail.com>
2019-12-11 10:36:48 -0800

    Initialize to do app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CreateTodo from './components/CreateTodo';
+import EditTodo from './components/EditTodo';
+import TodosList from './components/TodosList';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      {/* <h1>Your Todo List Today</h1> */}
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
     </div>
+    </Router>
   );
 }
 
